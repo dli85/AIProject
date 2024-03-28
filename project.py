5f802b6738b67080256a169510680c7d5dfb952a
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2024-03-28 16:26:10 -0400

    imported dot env into project

--- project.py
+++ project.py
@@ -14,10 +14,14 @@ from alpha_vantage.timeseries import TimeSeries
 
 import pandas as pd
 import pickle
+import os
+
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 
 print("All libraries loaded")
 
-API_KEY = "AUQ0TSPNKF2SX95I"
+API_KEY = os.getenv('MY_API_KEY')
 
 config = {
     "alpha_vantage": {
@@ -88,15 +92,15 @@ print(data_date[0], data_close_price[0])
 
 # plot
 
-if False:
-    fig = figure(figsize=(25, 5), dpi=80)
-    fig.patch.set_facecolor((1.0, 1.0, 1.0))
-    plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
-    xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
-    x = np.arange(0,len(xticks))
-    plt.xticks(x, xticks, rotation='vertical')
-    plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
-    plt.show()
+# if False:
+fig = figure(figsize=(25, 5), dpi=80)
+fig.patch.set_facecolor((1.0, 1.0, 1.0))
+plt.plot(data_date, data_close_price, color=config["plots"]["color_actual"])
+xticks = [data_date[i] if ((i%config["plots"]["xticks_interval"]==0 and (num_data_points-i) > config["plots"]["xticks_interval"]) or i==num_data_points-1) else None for i in range(num_data_points)] # make x ticks nice
+x = np.arange(0,len(xticks))
+plt.xticks(x, xticks, rotation='vertical')
+plt.title("Daily close price for " + config["alpha_vantage"]["symbol"] + ", " + display_date_range)
+plt.show()
 
 
 class Normalizer():
