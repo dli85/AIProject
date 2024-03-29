8472c1401cefc14176ed14553d6d24d89760aa34
aneeshponduru <aneesh.ponduru@gmail.com>
2024-03-29 19:10:30 -0400

    'income statement'

--- APIs/income_statement.py
+++ APIs/income_statement.py
@@ -1,9 +1,14 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 BASE_AV_URL = 'https://www.alphavantage.co/query'
 
+load_dotenv()
+
 def get_income_statement(ticker):
+
+    return_dict_list = []
 
     params = {
         'function': 'INCOME_STATEMENT',
@@ -15,4 +20,14 @@ def get_income_statement(ticker):
 
     if response.status_code == 200:
         data = response.json()
-        return data['quarterlyReports']
+        for i in range(len(data['quarterlyReports'])):
+            return_dict = {}
+            return_dict['fiscalDateEnding'] = data['quarterlyReports'][i]['fiscalDateEnding']
+            return_dict['grossProfit'] = data['quarterlyReports'][i]['grossProfit']
+            return_dict['totalRevenue'] = data['quarterlyReports'][i]['totalRevenue']
+            return_dict_list.append(return_dict)
+        return return_dict_list
+
+
+if __name__ == "__main__":
+    print(get_income_statement('AAPL'))
