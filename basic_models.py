6a67b704a96a53152956581fbe8b943ddbd53a86
David Li <dali7758769@gmail.com>
2024-04-02 14:45:35 -0400

    basic models working

--- basic_models.py
+++ basic_models.py
@@ -106,12 +106,12 @@ def split(stock):
     # x_train[i] = some sequence of prices
     # y_train[i] = the next price
 
-    plt.plot(y_train, color='blue', marker='*', linestyle='--')
-    plt.xlabel("Index")
-    plt.ylabel("Values")
-    plt.title("Data Plot")
-    plt.grid(True)
-    plt.show()
+    # plt.plot(y_train, color='blue', marker='*', linestyle='--')
+    # plt.xlabel("Index")
+    # plt.ylabel("Values")
+    # plt.title("Data Plot")
+    # plt.grid(True)
+    # plt.show()
 
     return [x_train, y_train, x_test, y_test]
 
@@ -154,17 +154,22 @@ def eval_model(model, y_train, train_predictions, x_test, y_test, scaler):
     return y_train, y_test, train_predictions, testing_predictions
 
 
-def plot_results(y_train, y_test, train_predictions, testing_predictions, history, dates):
-    df = pd.DataFrame({'Actual': np.concatenate((y_train.flatten(), y_test.flatten())),  # Concatenate
-                       'Predicted': np.concatenate((train_predictions.flatten(), testing_predictions.flatten()))})
+def plot_results(y_train, y_test, lstm_training_predictions, lstm_testing_predictions,
+                 gru_train_predictions, gru_testing_predictions, lstm_history, gru_history, dates):
 
-    split_index = int(training_split * (len(train_predictions) + len(testing_predictions)))
+
+    # lstm graphs
+    df = pd.DataFrame({'Actual': np.concatenate((y_train.flatten(), y_test.flatten())),
+                       'Predicted': np.concatenate((lstm_training_predictions.flatten(),
+                                                    lstm_testing_predictions.flatten()))})
+
+    split_index = int(training_split * (len(lstm_training_predictions) + len(lstm_testing_predictions)))
     predicted_1 = df['Predicted'].iloc[:split_index]
     predicted_2 = df['Predicted'].iloc[split_index:]
 
     fig = plt.figure()
     sns.set_style("darkgrid")
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     fig.subplots_adjust(hspace=0.2, wspace=0.2)
     sns.lineplot(data=df, x=df.index, y='Actual', label='Actual')
     sns.lineplot(x=predicted_1.index, y=predicted_1, label='Predicted Training', color='blue')
@@ -172,43 +177,56 @@ def plot_results(y_train, y_test, train_predictions, testing_predictions, histor
 
     plt.xlabel("Time Step")
     plt.ylabel("Stock Price")
-    plt.title("Stock Price Predictions")
+    plt.title("LSTM Predictions")
     plt.legend()
 
-    plt.subplot(1, 2, 2)
-    ax = sns.lineplot(data=history, color='royalblue')
+    plt.subplot(2, 2, 2)
+    ax = sns.lineplot(data=lstm_history, color='royalblue')
     ax.set_xlabel("Epoch", size=14)
     ax.set_ylabel("Loss", size=14)
-    ax.set_title("Training Loss", size=14, fontweight='bold')
+    ax.set_title("LSTM Training Loss", size=14, fontweight='bold')
     fig.set_figheight(6)
+    fig.set_figwidth(16)
+
+
+    # GRU graphs
+    df = pd.DataFrame({'Actual': np.concatenate((y_train.flatten(), y_test.flatten())),
+                       'Predicted': np.concatenate((gru_train_predictions.flatten(), gru_testing_predictions.flatten()))})
+
+    split_index = int(training_split * (len(gru_train_predictions) + len(gru_testing_predictions)))
+    predicted_1 = df['Predicted'].iloc[:split_index]
+    predicted_2 = df['Predicted'].iloc[split_index:]
+
+    sns.set_style("darkgrid")
+    plt.subplot(2, 2, 3)
+    fig.subplots_adjust(hspace=0.2, wspace=0.2)
+    sns.lineplot(data=df, x=df.index, y='Actual', label='Actual')
+    sns.lineplot(x=predicted_1.index, y=predicted_1, label='Predicted Training', color='blue')
+    sns.lineplot(x=predicted_2.index, y=predicted_2, label='Predicted Testing', color='red')
+
+    plt.xlabel("Time Step")
+    plt.ylabel("Stock Price")
+    plt.title("GRU Predictions")
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    ax = sns.lineplot(data=gru_history, color='royalblue')
+    ax.set_xlabel("Epoch", size=14)
+    ax.set_ylabel("Loss", size=14)
+    ax.set_title("GRU Training Loss", size=14, fontweight='bold')
+    fig.set_figheight(10)
     fig.set_figwidth(16)
 
     plt.show()
 
 
 def predict_next_n_days(model, n, x_test, y_test, scaler, sequence_length):
-    """
-    Predicts the stock prices for the next 30 days.
-
-    Args:
-        model (nn.Module): The trained LSTM or GRU model.
-        y_test (np.ndarray): The most recent 20% of close prices (used as starting point).
-        scaler (MinMaxScaler): The scaler used for preprocessing.
-        sequence_length (int): The sequence length used in model training
-
-    Returns:
-        list: A list of predicted stock prices for the next 30 days.
-    """
-
     def shift(arr, new_elem):
         """
-        Modifies a NumPy array by removing the first element and adding 1 to the end.
-
-        Args:
-            arr: A NumPy array with the specified shape.
-
-        Returns:
-            A new NumPy array with the modifications.
+        Shifts an array to the right by 1: removes the first element and adds an element at the end
+        :param arr: The arr to shift
+        :param new_elem: Element to be added. Should be in the shape [number]
+        :return: THe shifted array
         """
         modified_arr = [arr[0][1:]]
         # Create a new array to hold the element to be added
@@ -218,8 +236,7 @@ def predict_next_n_days(model, n, x_test, y_test, scaler, sequence_length):
 
         return modified_arr
 
-    # create the window
-    # window = most recent sequence of prices
+    # create the window: most recent sequence of prices
     x_test = x_test.numpy()
     window = [x_test[-1]]
     window = shift(window, y_test[-1])
@@ -240,18 +257,32 @@ def predict_next_n_days(model, n, x_test, y_test, scaler, sequence_length):
 
 
 if __name__ == '__main__':
-    df_date_close, dates = get_data_frame('INTC')
+    df_date_close, dates = get_data_frame('NOK')
 
     prices, scaler = preprocess(df_date_close)
     x_train, y_train, x_test, y_test = split(prices)
 
-    y_test_copy = np.copy(y_test)
+    y_train_initial = np.copy(y_train)
+    y_test_initial = np.copy(y_test)
+
+    x_train_for_lstm = torch.from_numpy(x_train).type(torch.Tensor)
+    x_test_for_lstm = torch.from_numpy(x_test).type(torch.Tensor)
+
+    x_train_for_gru = torch.from_numpy(x_train).type(torch.Tensor)
+    x_test_for_gru = torch.from_numpy(x_test).type(torch.Tensor)
 
     x_train = torch.from_numpy(x_train).type(torch.Tensor)
     x_test = torch.from_numpy(x_test).type(torch.Tensor)
 
-    model, training_prediction, history, = train(GRU(), x_train, y_train)
-    y_train, y_test, train_predictions, testing_predictions = \
-        eval_model(model, y_train, training_prediction, x_test, y_test, scaler)
-    plot_results(y_train, y_test, train_predictions, testing_predictions, history, dates)
-    predict_next_n_days(model, 30, x_test, y_test_copy, scaler, sequence_length)
+    lstm, lstm_training_predictions, lstm_history, = train(LSTM(), x_train_for_lstm, y_train)
+    y_train_for_lstm, y_test_for_lstm, lstm_train_prediction, lstm_testing_predictions = \
+        eval_model(lstm, y_train, lstm_training_predictions, x_test, y_test, scaler)
+
+    gru, gru_training_prediction, gru_history, = train(GRU(), x_train_for_gru, y_train)
+    y_train_for_gru, y_test_for_gru, gru_train_predictions, gru_testing_predictions = \
+        eval_model(gru, y_train, gru_training_prediction, x_test, y_test, scaler)
+
+    plot_results(scaler.inverse_transform(np.copy(y_train_initial)), scaler.inverse_transform(np.copy(y_test_initial)),
+                 lstm_train_prediction, lstm_testing_predictions, gru_train_predictions, gru_testing_predictions,
+                 lstm_history, gru_history, dates)
+    predict_next_n_days(gru, 30, x_test, np.copy(y_test_initial), scaler, sequence_length)
